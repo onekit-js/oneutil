86f554ae0b888477d8da0f2bfafd91f5f6f748fe
zhangjinva <zhangjin_va@qq.com>
2020-11-18 21:37:06 +0800

    。

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import COLOR from './COLOR'
-import PATH from './PATH'
-import STRING from './STRING'
-import URL from './URL'
-import DATE from './DATE'
-import HTML from './HTML'
 import TheKit from './TheKit'
 
+import COLOR from './COLOR'
+import DATE from './DATE'
+import HTML from './HTML'
+import PATH from './PATH'
+import PROMISE from './PROMISE'
+import STRING from './STRING'
+import URL from './URL'
+
 export {
-  COLOR, PATH, STRING, URL, DATE, HTML, TheKit
+  TheKit, COLOR, DATE, HTML, PATH, PROMISE, STRING, URL
 }

7d3a123021cab9bc37e0c831d058b31ea13b8283
15802303637 <>
2021-01-09 14:58:03 +0800

    .

--- OBJECT.js
+++ OBJECT.js
@@ -1,11 +1,17 @@
-export default class OBJECT{
-    static assign(object1,object2){
-        for(const key of Object.keys(object1)){
+export default class OBJECT {
+    static assign(object1, object2) {
+        if (object2 == null) {
+            return object1
+        }
+        for (const key of Object.keys(object2)) {
             const value1 = object1[key]
             const value2 = object2[key]
-            if(value1!=null && value2!=null){
-                object1[key] = this.assign(value1,value2)
-            }else if(value2!=null){
+            if (value2 == null) {
+                continue;
+            }
+            if (value1 != null) {
+                object1[key] = this.assign(value1, value2)
+            } else {
                 object1[key] = value2
             }
         }
